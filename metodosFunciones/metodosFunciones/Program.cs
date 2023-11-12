e640dd6157542d7208f8df46de588a02bd3b4805
govandoh <92902426+govandoh@users.noreply.github.com>
2023-11-12 14:04:47 -0600

    Carpeta metodos

---
+++ metodosFunciones/metodosFunciones/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metodosFunciones
+{
+    internal class Program
+    {
+
+        static void Saludos(String Nombre)
+        {
+            Console.WriteLine("Hola Mundo " + Nombre + ", Gusto en saludarte");
+            //String saludo = "Hola Mundo" + Nombre + " Gusto en saludarte";
+            //return saludo;
+        }
+
+        static void Main(string[] args)
+        {
+            Saludos("Juan");
+            Console.ReadKey();
+        }
+        
+    }
+}
