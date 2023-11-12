dd1ec38e83d5e4eb5e8d1cb75ca5e3e3c290ff9e
govandoh <92902426+govandoh@users.noreply.github.com>
2023-11-12 14:30:09 -0600

    Examen Final
    Codigo creado para el examen final

---
+++ ExamenFinal/ExamenFinal/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace ExamenFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn__guardar = new System.Windows.Forms.Button();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.btn_actualizar = new System.Windows.Forms.Button();
+            this.txt_Carne = new System.Windows.Forms.TextBox();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_celular = new System.Windows.Forms.TextBox();
+            this.lts_alumnos = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.principalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.actualizarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Celular";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Carne";
+            // 
+            // btn__guardar
+            // 
+            this.btn__guardar.Location = new System.Drawing.Point(460, 175);
+            this.btn__guardar.Name = "btn__guardar";
+            this.btn__guardar.Size = new System.Drawing.Size(75, 23);
+            this.btn__guardar.TabIndex = 4;
+            this.btn__guardar.Text = "Guardar";
+            this.btn__guardar.UseVisualStyleBackColor = true;
+            this.btn__guardar.Click += new System.EventHandler(this.btn__guardar_Click);
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Location = new System.Drawing.Point(565, 175);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(75, 23);
+            this.btn_eliminar.TabIndex = 5;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            this.btn_eliminar.Click += new System.EventHandler(this.btn_eliminar_Click);
+            // 
+            // btn_actualizar
+            // 
+            this.btn_actualizar.Location = new System.Drawing.Point(663, 175);
+            this.btn_actualizar.Name = "btn_actualizar";
+            this.btn_actualizar.Size = new System.Drawing.Size(75, 23);
+            this.btn_actualizar.TabIndex = 6;
+            this.btn_actualizar.Text = "Actualizar";
+            this.btn_actualizar.UseVisualStyleBackColor = true;
+            this.btn_actualizar.Click += new System.EventHandler(this.btn_actualizar_Click);
+            // 
+            // txt_Carne
+            // 
+            this.txt_Carne.Location = new System.Drawing.Point(115, 52);
+            this.txt_Carne.Multiline = true;
+            this.txt_Carne.Name = "txt_Carne";
+            this.txt_Carne.Size = new System.Drawing.Size(186, 20);
+            this.txt_Carne.TabIndex = 7;
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(114, 95);
+            this.txt_nombre.Multiline = true;
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(187, 20);
+            this.txt_nombre.TabIndex = 8;
+            // 
+            // txt_celular
+            // 
+            this.txt_celular.Location = new System.Drawing.Point(115, 143);
+            this.txt_celular.Multiline = true;
+            this.txt_celular.Name = "txt_celular";
+            this.txt_celular.Size = new System.Drawing.Size(186, 20);
+            this.txt_celular.TabIndex = 9;
+            // 
+            // lts_alumnos
+            // 
+            this.lts_alumnos.FormattingEnabled = true;
+            this.lts_alumnos.Location = new System.Drawing.Point(357, 49);
+            this.lts_alumnos.Name = "lts_alumnos";
+            this.lts_alumnos.Size = new System.Drawing.Size(480, 108);
+            this.lts_alumnos.TabIndex = 10;
+            this.lts_alumnos.SelectedIndexChanged += new System.EventHandler(this.lts_alumnos_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.principalToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(888, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // principalToolStripMenuItem
+            // 
+            this.principalToolStripMenuItem.Name = "principalToolStripMenuItem";
+            this.principalToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.principalToolStripMenuItem.Text = "Principal";
+            this.principalToolStripMenuItem.Click += new System.EventHandler(this.principalToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(41, 20);
+            this.toolStripMenuItem1.Text = "Salir";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.actualizarToolStripMenuItem,
+            this.eliminarToolStripMenuItem});
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(100, 20);
+            this.toolStripMenuItem2.Text = "Modificaciones";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // actualizarToolStripMenuItem
+            // 
+            this.actualizarToolStripMenuItem.Name = "actualizarToolStripMenuItem";
+            this.actualizarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.actualizarToolStripMenuItem.Text = "Actualizar";
+            this.actualizarToolStripMenuItem.Click += new System.EventHandler(this.actualizarToolStripMenuItem_Click);
+            // 
+            // eliminarToolStripMenuItem
+            // 
+            this.eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
+            this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.eliminarToolStripMenuItem.Text = "Eliminar";
+            this.eliminarToolStripMenuItem.Click += new System.EventHandler(this.eliminarToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 210);
+            this.Controls.Add(this.lts_alumnos);
+            this.Controls.Add(this.txt_celular);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.txt_Carne);
+            this.Controls.Add(this.btn_actualizar);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.btn__guardar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn__guardar;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.Button btn_actualizar;
+        private System.Windows.Forms.TextBox txt_Carne;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_celular;
+        private System.Windows.Forms.ListBox lts_alumnos;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem principalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem actualizarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarToolStripMenuItem;
+    }
+}
+
---
+++ ExamenFinal/ExamenFinal/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamenFinal
+{
+    public partial class Form1 : Form
+    {
+        string archivoAlumnos = "archivoAlumnos.txt";
+        public Form1()
+        {
+            InitializeComponent();
+            CargarAlumnos();
+            lts_alumnos.SelectedIndexChanged += lts_alumnos_SelectedIndexChanged;
+        }
+
+        public void CargarAlumnos()
+        {
+            lts_alumnos.Items.Clear();
+            if (File.Exists(archivoAlumnos))
+            {
+                using (StreamReader reader = new StreamReader(archivoAlumnos))
+                {
+                    string lineas;
+
+                    while ((lineas = reader.ReadLine()) != null)
+                    {
+                        string[] linea = lineas.Split(',');
+                        Alumno alumno = new Alumno
+                        {
+                            Carne = linea[0],
+                            Nombre = linea[1],
+                            Celular = linea[2],
+                        };
+                        lts_alumnos.Items.Add(alumno);
+                    }
+                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("Archivo no existia, se creo uno nuevo");
+            }
+
+        }
+
+
+
+        private void lts_alumnos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lts_alumnos.SelectedItem != null)
+            {
+                Alumno alumno = (Alumno)lts_alumnos.SelectedItem;
+                txt_Carne.Text = alumno.Carne.ToString();
+                txt_nombre.Text = alumno.Nombre.ToString();
+                txt_celular.Text = alumno.Celular.ToString();
+            }
+        }
+
+        public void limpiarCtls()
+        {
+            txt_Carne.Clear();
+            txt_nombre.Clear();
+            txt_celular.Clear();
+        }
+
+        private void btn__guardar_Click(object sender, EventArgs e)
+        {
+            string carne; string nombre; string celular;
+            carne = txt_Carne.Text;
+            nombre = txt_nombre.Text;
+            celular = txt_celular.Text;
+            
+            if (string.IsNullOrEmpty(carne) || string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(celular))
+            {
+                MessageBox.Show("Datos faltantes, valide datos", "Error");
+
+            }
+            else
+            {
+                using (StreamWriter writer = new StreamWriter(archivoAlumnos,true))
+                {
+                    writer.WriteLine($"{carne},{nombre},{celular}");
+                }
+                CargarAlumnos();
+                limpiarCtls();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_eliminar_Click(object sender, EventArgs e)
+        {
+            if(lts_alumnos.SelectedItem  != null)
+            {
+                Alumno alumno = (Alumno)lts_alumnos.SelectedItem; 
+                string[] dataEliminar = File.ReadAllLines(archivoAlumnos);
+
+                dataEliminar = dataEliminar.Where(x => !x.Contains(alumno.Carne)).ToArray();
+                File.WriteAllLines(archivoAlumnos, dataEliminar);
+                CargarAlumnos();
+                limpiarCtls();
+            }
+            else
+            {
+                MessageBox.Show("Primero seleccione el registro a eliminar");
+            }
+            
+        }
+
+        private void btn_actualizar_Click(object sender, EventArgs e)
+        {
+            if(lts_alumnos.SelectedItem != null) 
+            {
+                Alumno alumno = (Alumno)lts_alumnos.SelectedItem;
+                string carne; string nombre; string celular;
+                carne = txt_Carne.Text;
+                nombre = txt_nombre.Text;
+                celular = txt_celular.Text;
+
+                if (string.IsNullOrEmpty(carne) || string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(celular))
+                {
+                    MessageBox.Show("Datos faltantes, valide datos", "Error");
+                }
+                else
+                {
+                    string[] dataUpdate = File.ReadAllLines(archivoAlumnos);
+                    string lineaCambios;
+                    lineaCambios = $"{carne},{nombre},{celular}";
+
+                    for(int i = 0; i < dataUpdate.Length; i++)
+                    {
+                        if (dataUpdate[i].Contains(alumno.Carne))
+                        {
+                            dataUpdate[i] = lineaCambios; 
+                        }
+                    }
+                    File.WriteAllLines(archivoAlumnos,dataUpdate);
+                    CargarAlumnos();
+                    limpiarCtls();
+                }
+            }
+
+            
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void actualizarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btn__guardar.Enabled = false; 
+            btn_eliminar.Enabled = false;
+            btn_actualizar.Enabled = true;
+            txt_Carne.Enabled = false;
+        }
+
+        private void eliminarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btn__guardar.Enabled=false;
+            btn_actualizar.Enabled=false;
+            btn_eliminar.Enabled = true;
+
+        }
+
+        private void principalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btn__guardar.Enabled = true;
+            txt_Carne.Enabled = true;
+            btn_eliminar.Enabled = false;
+            btn_actualizar.Enabled = false;
+        }
+    }
+
+    public class Alumno
+    {
+        public string Carne { get; set; }
+        public string Nombre { get; set; }
+        public string Celular { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Carne},{Nombre},{Celular}";
+        }
+    }
+}
